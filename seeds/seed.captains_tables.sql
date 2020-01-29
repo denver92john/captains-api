1c4bd9b5843649a6e4385d463212e6741d1691f2
Burl Adkins <adkins1393@gmail.com>
2020-01-29 09:38:46 -0500

    seeded db

---
+++ seeds/seed.captains_tables.sql
@@ -0,0 +1,29 @@
+BEGIN;
+
+TRUNCATE 
+    captains_items,
+    captains_lists,
+    captains_users
+    RESTART IDENTITY CASCADE;
+
+INSERT INTO captains_users (username, password)
+VALUES
+    ('JDenver', 'JDenver1!');
+
+INSERT INTO captains_lists (list_name, user_id)
+VALUES
+    ('New Years Eve', '1'),
+    ('Turkey Bowl', '1');
+
+INSERT INTO captains_items (item_name, list_id)
+VALUES
+    ('Bill', '1'),
+    ('Tyler', '1'),
+    ('Cline', '1'),
+    ('Ann', '1'),
+    ('Cormac', '2'),
+    ('Ava', '2'),
+    ('Ken', '2'),
+    ('Joe', '2');
+
+COMMIT;
---
+++ seeds/trunc.captains_tables.sql
@@ -0,0 +1,5 @@
+TRUNCATE
+    captains_items,
+    captains_lists,
+    captains_users
+    RESTART IDENTITY CASCADE;
