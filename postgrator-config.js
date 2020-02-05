7ea0e5072d671cfd789fa1867f4758ff4de5ae62
Burl Adkins <adkins1393@gmail.com>
2020-02-04 21:20:45 -0500

    ready to be deployed

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 module.exports = {
     "migrationDirectory": "migrations",
     "driver": "pg",
-    "connectionString": process.env.DATABASE_URL
+    "connectionString": process.env.DATABASE_URL,
+    "ssl": !!process.env.SSL
 }
 
 /*
