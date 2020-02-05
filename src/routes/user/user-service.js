222314fdd9724879ea018c00ca5afb2c5a53364c
Burl Adkins <adkins1393@gmail.com>
2020-02-05 15:57:34 -0500

    edited UserService.validatePassword to return obj with message as key

--- src/routes/user/user-service.js
+++ src/routes/user/user-service.js
@@ -5,16 +5,16 @@ const bcrypt = require('bcryptjs');
 const UserService = {
     validatePassword(password) {
         if(password.length < 8) {
-            return 'Password must be longer than 8 characters'
+            return {message: 'Password must be longer than 8 characters'}
         }
         if(password.length > 72) {
-            return 'Password must be less than 72 characters'
+            return {message: 'Password must be less than 72 characters'}
         }
         if(password.startsWith(' ') || password.endsWith(' ')) {
-            return 'Password must not start or end with empty spaces'
+            return {message: 'Password must not start or end with empty spaces'}
         }
         if(!REGEX_UPPER_LOWER_NUMBER_SPECIAL.test(password)) {
-            return 'Password must contain 1 upper case, lower case, number and special character'
+            return {message: 'Password must contain 1 upper case, lower case, number and special character'}
         }
         return null
     },
