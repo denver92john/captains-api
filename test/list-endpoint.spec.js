fea62bd9b8b477f8e3e86f5231ff9da32cb24498
Burl Adkins <adkins1393@gmail.com>
2020-02-15 16:50:54 -0500

    added delete test

--- test/list-endpoint.spec.js
+++ test/list-endpoint.spec.js
@@ -61,9 +61,23 @@ describe('List endpoint', () => {
         })
     })
 
-    describe('POST /api/list', () => {})
-    /*
+    //describe('POST /api/list', () => {})
+    
     describe('GET /api/list/:list_id', () => {
+        context('Given no list', () => {
+            beforeEach(() => 
+                helpers.seedUser(db, testUser)
+            )
+
+            it('responds 404', () => {
+                const list_id = 1234;
+                return supertest(app)
+                    .get(`/api/list/${list_id}`)
+                    .set('Authorization', helpers.makeAuthHeader(testUser))
+                    .expect(404, {error: {message: `List doesn't exist`}})
+            })
+        })
+
         context('Given there is a list', () => {
             beforeEach('insert lists', () => {
                 helpers.seedCaptainsTables(
@@ -77,17 +91,55 @@ describe('List endpoint', () => {
             it('responds with 200 and specific list', () => {
                 const list_id = 1;
                 const expectedList = helpers.makeExpectedList(testUser, testLists[list_id - 1])
-                console.log(expectedList)
-                console.log(testLists[list_id -1])
                 return supertest(app)
                     .get(`/api/list/${list_id}`)
                     .set('Authorization', helpers.makeAuthHeader(testUser))
                     .expect(200, expectedList)
             })
         })
-    })*/
+    })
 
-    describe('DELETE /api/list/:list_id', () => {})
+    describe('DELETE /api/list/:list_id', () => {
+        context('Given no list', () => {
+            beforeEach(() => 
+                helpers.seedUser(db, testUser)
+            )
 
-    describe('PATCH /api/list/:list_id', () => {})
+            it('responds with 404', () => {
+                const list_id = 1234;
+                return supertest(app)
+                    .delete(`/api/list/${list_id}`)
+                    .set('Authorization', helpers.makeAuthHeader(testUser))
+                    .expect(404, {error: {message: `List doesn't exist`}})
+            })
+        })
+
+        context('Given the list exist', () => {
+            beforeEach('insert lists', () => {
+                helpers.seedCaptainsTables(
+                    db,
+                    testUser,
+                    testLists,
+                    testItems
+                )
+            })
+
+            it('responds with 204 and removes list', () => {
+                const idToRemove = 2;
+                const expectedLists = testLists.filter(list => list.id !== idToRemove);
+                return supertest(app)
+                    .delete(`/api/list/${idToRemove}`)
+                    .set('Authorization', helpers.makeAuthHeader(testUser))
+                    .expect(204)
+                    .then(() => 
+                        supertest(app)
+                            .get(`/api/list`)
+                            .set('Authorization', helpers.makeAuthHeader(testUser))
+                            .expect(expectedLists)
+                    )
+            })
+        })
+    })
+
+    //describe('PATCH /api/list/:list_id', () => {})
 })
